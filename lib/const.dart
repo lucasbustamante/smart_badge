62d243e1b109978f04ca1f53f16117cb4ca8bcaf
toshiossada <lucascostabustamante@gmail.com>
2022-12-22 21:55:43 -0300

    atualização 22.12.22

--- lib/const.dart
+++ lib/const.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 
-const kPrimaryColor = Color(0xff7a0bb4);
+const kPrimaryColor = Color(0xff8800d0);
 const kSecondaryColor = Color(0xffa4a2a5);
 
 String name = 'Lucas Bustamante';
